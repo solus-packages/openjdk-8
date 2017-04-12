087bc1a328e5e75e15ab912670a3554c0b4844a4
Ikey Doherty <ikey@solus-project.com>
2017-04-12 02:37:13 +0100

    Add new script for preparing source tarballs
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+VERSION="121"
+REVISION="b13"
+BASEURI="http://hg.openjdk.java.net/jdk8u/jdk8u/archive"
+set -e
+
+# Main tarball
+wget -nc "${BASEURI}/jdk8u${VERSION}-${REVISION}.tar.bz2"
+
+function fetch_and_emit()
+{
+    wget -nc http://hg.openjdk.java.net/jdk8u/jdk8u/$1/archive/jdk8u${VERSION}-${REVISION}.tar.bz2 -O $1-u${VERSION}-${REVISION}.tar.bz2
+}
+
+# subproject tarballs
+for i in corba hotspot jaxp jaxws jdk langtools nashorn; do
+    fetch_and_emit $i
+done
+
+sha256sum *tar.bz2
