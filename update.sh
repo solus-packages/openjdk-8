2e45af5322b4512ce6c3d17a95753839c9589252
Campbell Jones <git@serebit.com>
2021-04-23 07:23:01 -0400

    Modify openjdk-8's update.sh script to generate a single tarball for upload
    Summary: This simplifies maintenance and load on the core team.
    
    Test Plan: Generated a new tarball for JDK 8u282
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Differential Revision: https://dev.getsol.us/D10395

--- update.sh
+++ update.sh
@@ -2,14 +2,30 @@
 VERSION="$1"
 REVISION="ga"
 BASEURI="http://hg.openjdk.java.net/jdk8u/jdk8u"
+TARGETDIR="jdk8u${VERSION}-${REVISION}"
 set -e
 
+if [[ -z $VERSION ]]; then
+    echo "Usage: $0 <version number>"
+    exit 1
+fi
+
 # Main tarball
-wget -nc "${BASEURI}/jdk8u${VERSION}-${REVISION}.tar.bz2"
+wget -nv -nc "${BASEURI}/archive/jdk8u${VERSION}-${REVISION}.tar.bz2"
+sha256sum jdk8u${VERSION}-${REVISION}.tar.bz2
+mkdir ${TARGETDIR}
+tar -xf jdk8u${VERSION}-${REVISION}.tar.bz2 --strip-components=1 -C ${TARGETDIR}
+rm -f jdk8u${VERSION}-${REVISION}.tar.bz2
+echo
 
 function fetch_and_emit()
 {
-    wget -nc ${BASEURI}/$1/archive/jdk8u${VERSION}-${REVISION}.tar.bz2 -O $1-u${VERSION}-${REVISION}.tar.bz2
+    wget -nv -nc ${BASEURI}/$1/archive/jdk8u${VERSION}-${REVISION}.tar.bz2 -O $1-u${VERSION}-${REVISION}.tar.bz2
+    sha256sum $1-u${VERSION}-${REVISION}.tar.bz2
+    mkdir -p ${TARGETDIR}/$1
+    tar -xf $1-u${VERSION}-${REVISION}.tar.bz2 --strip-components=1 -C ${TARGETDIR}/$1
+    rm -f $1-u${VERSION}-${REVISION}.tar.bz2
+    echo
 }
 
 # subproject tarballs
@@ -17,4 +33,7 @@ for i in corba hotspot jaxp jaxws jdk langtools nashorn; do
     fetch_and_emit $i
 done
 
-sha256sum *tar.bz2
+tar -cf jdk8u${VERSION}-${REVISION}-all.tar.gz ${TARGETDIR}
+rm -rf ${TARGETDIR}
+echo
+sha256sum jdk8u${VERSION}-${REVISION}-all.tar.gz
